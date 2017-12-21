c2cb28acf6b1c5d4b37d3e54d8378ec2eba9629c
Justin <cong.itsoft@gmail.com>
2017-12-21 18:01:39 +0700

    first commit

---
+++ src/components/Todo.vue
@@ -0,0 +1,16 @@
+<template lang="jade">
+  p Chao cong
+    a(href="/123") Link ne
+
+</template>
+
+<script>
+  export default {
+    name: 'Todo',
+    data () {
+      return {
+        name: 'hello'
+      }
+    }
+  }
+</script>
